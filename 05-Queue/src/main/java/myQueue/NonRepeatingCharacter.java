38e9df2d534f1cbff6395ca6f3af63576e1c6f33
VitalySlabada <vitaly.slabada@gmail.com>
2022-03-20 08:49:03 -0500

    Assignments: LRU

---
+++ 05-Queue/src/main/java/myQueue/NonRepeatingCharacter.java
@@ -0,0 +1,47 @@
+package myQueue;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NonRepeatingCharacter {
+    public static void main(String[] args) {
+        String input = "a green apple";
+
+        System.out.println(findFirstNonRepeatingCharachter(input));
+
+
+    }
+
+    private static char findFirstNonRepeatingCharachter(String str) {
+        Map<Character,Integer> map = new HashMap<>();
+
+        int count = 0;
+        char[] chars = str.toCharArray();
+
+        for(Character ch:chars){
+            if (map.containsKey(ch)){
+
+                count = map.get(ch);
+                map.put(ch, count+1);
+
+            }else{
+                map.put(ch,1);
+
+            }
+
+
+            }
+
+        for (Character ch1: chars){
+            if (map.get(ch1)==1) return ch1;
+
+
+        }
+
+        return Character.MIN_VALUE;
+
+    }
+
+
+}
---
+++ OO-Assignments/src/main/java/LRU/AlgoQuestion1LRUCache.java
@@ -0,0 +1,16 @@
+package LRU;
+
+import java.util.LinkedHashMap;
+
+public class AlgoQuestion1LRUCache {
+    public static void main(String[] args) {
+        LRUCache lru=new LRUCache(3);
+        lru.put(1, 1);
+        lru.put(2, 2);
+        lru.put(3, 3);
+        System.out.println(lru.get(1));
+        lru.put(4, 4);
+        System.out.println(lru.get(2));
+
+    }
+}
